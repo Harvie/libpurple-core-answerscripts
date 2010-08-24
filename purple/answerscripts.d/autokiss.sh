3971900dcde58c50b85eabf0d4bcf5f20ba1114a
Harvie <tomas@mudrunka.cz>
2010-08-24 03:10:54 +0200

    Autokiss now shuffles if possible

--- purple/answerscripts.d/autokiss.sh
+++ purple/answerscripts.d/autokiss.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #AutoKiss :-* (just wanna be polite)
-echo $(echo "$ANSW_MSG" | grep -Eo ':-\*|\*IN LOVE\*|:-\{\}|\*KISSING\*');
+shuf="$(which shuf 2>/dev/null)"; [ -x "$shuf" ] || shuf="cat"; #shuffle if possible
+echo $(echo "$ANSW_MSG" | grep -Eo ':-\*|\*IN LOVE\*|:-\{\}|\*KISSING\*' | "$shuf" );
