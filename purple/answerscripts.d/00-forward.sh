2270485e8ffed1081f810d3352fd53d1b3ee2e6d
Harvie <tomas@mudrunka.cz>
2010-08-29 22:51:15 +0200

    New answerscript for forwarding messages

---
+++ purple/answerscripts.d/00-forward.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#This script will forward all incoming IM messages to someone else in network
+FORWARD_PROTOCOL='irc';
+FORWARD_ACCOUNT='Harvie@irc.freenode.net';
+FORWARD_TO='hrv';
+purple-remote "$FORWARD_PROTOCOL:goim?account=$FORWARD_ACCOUNT&screenname=$FORWARD_TO&message=""$(echo "<$ANSW_PROTOCOL:$ANSW_FROM> $ANSW_MSG" | tr '&' ' ')"
