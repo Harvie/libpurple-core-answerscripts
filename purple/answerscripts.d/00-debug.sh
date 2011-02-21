bd5b023fcefc39ebca7072d4eba57ee86818e33c
Harvie <tomas@mudrunka.cz>
2011-02-21 03:08:30 +0100

    Better 00-debug.sh

--- purple/answerscripts.d/00-debug.sh
+++ purple/answerscripts.d/00-debug.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #Debug
-echo "<$ANSW_PROTOCOL/$ANSW_FROM> $ANSW_MSG" >&2;
-echo "($ANSW_STATUS: $ANSW_STATUS_MSG)" >&2;
+(
+echo "DATE: $(date)";
+env | grep '^ANSW_';
+echo =============================================
+) >&2;
