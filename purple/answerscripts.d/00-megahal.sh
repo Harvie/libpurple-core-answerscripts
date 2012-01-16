d5bbca3f8e2ca1284552e2d89fd505dee0e7d987
Harvie <tomas@mudrunka.cz>
2012-01-16 23:04:38 +0100

    Added support for MegaHAL AI ChatterBot (Both: Learning and Conversation mode)

---
+++ purple/answerscripts.d/00-megahal.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#Artificial Pseudo Inteligence using megahal package :-)
+#More info: http://megahal.alioth.debian.org/
+#alias cat=true #silent mode, comment out to enable loud replies :-)
+echo -n '[MegaHAL]:' $({ echo "$ANSW_MSG" &&	echo -e '\n#quit\n';	} | megahal -p -b | tail -n1) | cat
