849af27c61f37313a7b8df74bcfa7f7e918f825e
Harvie <tomas@mudrunka.cz>
2012-01-16 23:39:07 +0100

    Megahal: do not wrap...

--- purple/answerscripts.d/00-megahal.sh
+++ purple/answerscripts.d/00-megahal.sh
@@ -2,4 +2,4 @@
 #Artificial Pseudo Inteligence using megahal package :-)
 #More info: http://megahal.alioth.debian.org/
 #alias cat=true #silent mode, comment out to enable loud replies :-)
-echo -n '[MegaHAL]:' $({ echo "$ANSW_MSG" &&	echo -e '\n#quit\n';	} | megahal -p -b | tail -n1) | cat
+echo -n '[MegaHAL]:' $({ echo "$ANSW_MSG" &&	echo -e '\n#quit\n';	} | megahal -p -b -w | tail -n1) | cat
