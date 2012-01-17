de517ba369532014d55667f27b6c5cedd722a5b2
Harvie <tomas@mudrunka.cz>
2012-01-17 06:16:40 +0100

    Very big commit fixing problems with czech letters in MegaHAL

--- purple/answerscripts.d/00-megahal.sh
+++ purple/answerscripts.d/00-megahal.sh
@@ -6,5 +6,5 @@ AI_LOUD=false #enable loud replies (otherwise will be learning only :-)
 AI_LOUD_UNAVAILABLE=true; #enable loud replies when not available :-)
 
 $AI_LOUD_UNAVAILABLE && [ "$ANSW_L_STATUS" != 'available' ] && AI_LOUD=true
-AI_ANSW="$({ echo "$ANSW_MSG" && echo -e '\n#quit\n'; } | timeout 23 megahal -p -b -w | tail -n1)"
+AI_ANSW="$({ echo "$ANSW_MSG." && echo -e '\n#quit\n'; } | timeout 23 megahal -p -b -w | tail -n1)"
 $AI_LOUD && [ -n "$AI_ANSW" ] && echo -n "[MegaHALuz]: $AI_ANSW"
