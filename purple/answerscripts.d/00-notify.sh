24aef432f43946d7d722cf429467c06d6ca2465b
Tomas Mudrunka <tomas@mudrunka.cz>
2015-02-19 16:44:41 +0100

    Escape notify

--- purple/answerscripts.d/00-notify.sh
+++ purple/answerscripts.d/00-notify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #Show notification for each incomming message
-notify-send -i pidgin "$ANSW_PROTOCOL:$ANSW_R_NAME" "$ANSW_MSG"
+notify-send -i pidgin "$ANSW_PROTOCOL:$ANSW_R_NAME" "$(echo "$ANSW_MSG" | sed -e 's/&/&amp;/g')"
