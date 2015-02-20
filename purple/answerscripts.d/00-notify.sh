d66a447d3cac8a9a40225a421b67fab1ebb1742c
Tomas Mudrunka <tomas@mudrunka.cz>
2015-02-20 15:25:46 +0100

    Better escaping for notify-send

--- purple/answerscripts.d/00-notify.sh
+++ purple/answerscripts.d/00-notify.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #Show notification for each incomming message
-notify-send -i pidgin "$ANSW_PROTOCOL:$ANSW_R_NAME" "$(echo "$ANSW_MSG" | sed -e 's/&/&amp;/g')"
+which recode >/dev/null && ANSW_MSG="$(echo "$ANSW_MSG" | recode ascii..html)"
+notify-send -i pidgin "$ANSW_PROTOCOL:$ANSW_R_NAME" "$ANSW_MSG"
