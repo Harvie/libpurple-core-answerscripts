c3eb58d2228fa552ce59aa84c8fc38b5dd842116
Tomas Mudrunka <tomas@mudrunka.cz>
2015-02-22 18:05:58 +0100

    simplified recoding of entities for notify-send

--- purple/answerscripts.d/00-notify.sh
+++ purple/answerscripts.d/00-notify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Show notification for each incomming message
-which recode >/dev/null && ANSW_MSG="$(echo "$ANSW_MSG" | recode ..html)"
+ANSW_MSG="$(echo "$ANSW_MSG" | sed -e 's/&/&amp\;/g;s/</\&lt\;/g;s/>/\&gt;/g')"
 notify-send -i pidgin "$ANSW_PROTOCOL:$ANSW_R_NAME" "$ANSW_MSG"
